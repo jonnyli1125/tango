8bac405186c100688eac22b1169599db9eb85673
Alec Jacob Deitloff <alec.deitloff@gmail.com>
2019-06-27 00:03:32 -0700

    run prettier on webpack.config.ts

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,3 +1,5 @@
+/* tslint:disable */
+
 import * as path from "path";
 import * as webpack from "webpack";
 
@@ -6,26 +8,40 @@ const FilterWarningsPlugin = require("webpack-filter-warnings-plugin");
 const commonConfig: webpack.Configuration = {
   devtool: "source-map",
   resolve: {
-      extensions: [".ts", ".tsx", ".js", ".json"]
+    extensions: [".ts", ".tsx", ".js", ".json"]
   },
   module: {
-      rules: [
-          { test: /\.tsx?$/, exclude: /node_modules/, loader: "awesome-typescript-loader" },
-          { test: /\.js$/, enforce: "pre", loader: "source-map-loader" },
-          { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] }
-      ]
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "awesome-typescript-loader"
+      },
+      { test: /\.js$/, enforce: "pre", loader: "source-map-loader" },
+      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] }
+    ]
   },
   output: {
-      filename: "[name].js",
-      path: __dirname + "/dist"
+    filename: "[name].js",
+    path: __dirname + "/dist"
   },
   node: {
     __dirname: false,
-    __filename: false,
+    __filename: false
   },
   plugins: [
     new FilterWarningsPlugin({
-      exclude: [/mongodb/, /mssql/, /mysql/, /mysql2/, /oracledb/, /pg/, /pg-native/, /pg-query-stream/, /redis/]
+      exclude: [
+        /mongodb/,
+        /mssql/,
+        /mysql/,
+        /mysql2/,
+        /oracledb/,
+        /pg/,
+        /pg-native/,
+        /pg-query-stream/,
+        /redis/
+      ]
     })
   ],
   externals: {
@@ -33,14 +49,20 @@ const commonConfig: webpack.Configuration = {
   }
 };
 
-const mainConfig = Object.assign({
-  entry: { main: path.resolve(__dirname, "src/main-process/main") },
-  target: "electron-main"
-}, commonConfig);
+const mainConfig = Object.assign(
+  {
+    entry: { main: path.resolve(__dirname, "src/main-process/main") },
+    target: "electron-main"
+  },
+  commonConfig
+);
 
-const rendererConfig = Object.assign({
-  entry: { renderer: path.resolve(__dirname, "src/ui/index") },
-  target: "electron-renderer"
-}, commonConfig);
+const rendererConfig = Object.assign(
+  {
+    entry: { renderer: path.resolve(__dirname, "src/ui/index") },
+    target: "electron-renderer"
+  },
+  commonConfig
+);
 
 export default [mainConfig, rendererConfig];
