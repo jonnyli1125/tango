d7d3936e34ca58173323b07267b06be58714eb98
Alec Jacob Deitloff <alec.deitloff@gmail.com>
2019-06-26 23:05:03 -0700

    switch to keeping stylesheets next to components

--- src/ui/components/Summary.tsx
+++ src/ui/components/Summary.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 
+import "./Summary.scss";
+
 export interface SummaryProps {
   compiler: string;
   framework: string;
@@ -8,9 +10,11 @@ export interface SummaryProps {
 export class Summary extends React.Component<SummaryProps> {
   public render() {
     return (
-      <h1>
-        Hello from {this.props.compiler} and {this.props.framework}!
-      </h1>
+      <div className="summary">
+        <h1>
+          Hello from {this.props.compiler} and {this.props.framework}!
+        </h1>
+      </div>
     );
   }
 }
